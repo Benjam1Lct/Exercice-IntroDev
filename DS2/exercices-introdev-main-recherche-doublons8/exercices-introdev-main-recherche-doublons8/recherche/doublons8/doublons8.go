bf8e035accb2016998a04b72eeb35639c0e3e22a
E230612Q <benjamin.lecomte@etu.univ-nantes.fr>
2023-11-24 12:19:37 +0100

    update

--- DS2/exercices-introdev-main-recherche-doublons8/exercices-introdev-main-recherche-doublons8/recherche/doublons8/doublons8.go
+++ DS2/exercices-introdev-main-recherche-doublons8/exercices-introdev-main-recherche-doublons8/recherche/doublons8/doublons8.go
@@ -1,5 +1,7 @@
 package doublons8
 
+import "fmt"
+
 /*
 On dispose d'un tableau d'entiers de longueur n et on suppose qu'il contient exactement p fois chaque nombre compris entre k et k + (n/p) - 1 (pas forcément dans l'ordre) pour un certain k et un certain p non connus à l'avance (p différent de 0). On voudrait vérifier que c'est bien le cas. C'est le travail de la fonction doublons.
 
@@ -14,5 +16,53 @@ On dispose d'un tableau d'entiers de longueur n et on suppose qu'il contient exa
 */
 
 func doublons(tab []int) (ok bool) {
-	return
+	for i := 0; i < len(tab); i++ {
+		for j := i + 1; j < len(tab); j++ {
+			if tab[i] > tab[j] {
+				tab[i], tab[j] = tab[j], tab[i]
+
+			}
+		}
+	}
+	fmt.Println(tab)
+
+	max_present := 0
+	for k := 0; k < len(tab); k++ {
+		num_present := 1
+		for i := 0; i < len(tab); i++ {
+			if tab[k] == tab[i] && k != i {
+				num_present += 1
+			}
+		}
+		if num_present > max_present {
+			max_present = num_present
+		}
+
+	}
+
+	fmt.Println(max_present)
+
+	var already_check []int = []int{}
+	for i := 0; i < len(tab); i++ {
+		verify := true
+		for k := 0; k < len(already_check); k++ {
+			if already_check[k] == i {
+				verify = false
+			}
+		}
+		if verify == true {
+			counter := 1
+			for j := i + 1; j < len(tab); j++ {
+				if tab[i] == tab[j] {
+					counter = counter + 1
+				}
+			}
+			if counter != max_present {
+				return false
+			}
+		}
+
+	}
+
+	return true
 }
